d64c6438c375a5aab2fdc4c983f0a9d2e645ef17
weiting <weitingmm@5xruby.com>
2023-02-12 21:13:11 +0800

    adjust animation page

--- src/views/work/Animation.vue
+++ src/views/work/Animation.vue
@@ -3,8 +3,15 @@
 </script>
 
 <template>
-    <div>
-
+    <div class="bg-[rgba(216,250,255,.5)] p-5">
+        <div class="flex flex-col mb-6 md:rounded-3xl p-5 w-full bg-[rgba(255,255,255,.5)]">
+            <div class="self-end mr-1.5 mb-3 text-sm">o o 組</div>
+            <div class="mb-5 w-full h-52 bg-white"></div>
+            <div class="flex justify-between">
+                <div class="ml-3 text-[#58595B] text-xl font-semibold">作品標題</div>
+                <div class="ml-3 border border-black rounded px-7 py-1.5 text-sm">V i e w</div>
+            </div>
+        </div>
     </div>
 </template>
 
