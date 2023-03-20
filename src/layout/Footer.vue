87c0bd5fb2008724e11994b6b9acc15d755a2735
weiting <weitingmm@5xruby.com>
2023-03-20 21:44:24 +0800

    add footer links

--- src/layout/Footer.vue
+++ src/layout/Footer.vue
@@ -7,13 +7,13 @@
         class="w-full flex sm:flex-col justify-center items-center py-4 lg:py-8 text-lg sm:text-sm text-[#FFFFFF] bg-[#595959]">
         <div class="px-10">
             <div class="flex justify-center items-center gap-x-6 mb-1">
-                <a href="" target="_blank" class="overflow-hidden">
+                <a href="https://www.facebook.com/yzuic26th" target="_blank" class="overflow-hidden">
                     <img src='/img/fb-footer.png' class="footer-icon w-6 object-contain -translate-x-10" />
                 </a>
                 <a href="" target="_blank" class="overflow-hidden">
                     <img src='/img/yt-footer.svg' class="footer-icon w-6 object-contain -translate-x-10" />
                 </a>
-                <a href="" target="_blank" class="overflow-hidden">
+                <a href="https://www.instagram.com/yzuic_26/" target="_blank" class="overflow-hidden">
                     <img src='/img/ig-footer.png' class="footer-icon w-5 object-contain -translate-x-10" />
                 </a>
             </div>
